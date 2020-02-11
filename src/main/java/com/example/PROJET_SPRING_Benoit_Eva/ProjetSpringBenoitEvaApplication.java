2ae4a685926a8e7d59c9865a14b743c79f5be8de
EvaSoba <eva.sobaco.morison@gmail.com>
2020-02-11 10:52:17 +0100

    hello orld

---
+++ src/main/java/com/example/PROJET_SPRING_Benoit_Eva/ProjetSpringBenoitEvaApplication.java
@@ -0,0 +1,15 @@
+package com.example.PROJET_SPRING_Benoit_Eva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringBenoitEvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSpringBenoitEvaApplication.class, args);
+		System.out.println("Hello world");
+		
+	}
+
+}
---
+++ src/test/java/com/example/PROJET_SPRING_Benoit_Eva/ProjetSpringBenoitEvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROJET_SPRING_Benoit_Eva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBenoitEvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
